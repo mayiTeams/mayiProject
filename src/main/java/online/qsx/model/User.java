3d311db8b76c746c664e8f5ed94810f09ee79c06
GoodRens <1660895535@qq.com>
2017-10-19 10:11:39 +0800

    表增加了，email，phone ，address等三列

--- src/main/java/online/qsx/model/User.java
+++ src/main/java/online/qsx/model/User.java
@@ -17,8 +17,11 @@ public class User {
 	private String username;
 	private String password;
 	private String balance;
+	private String email;
+	private String phone;
+	private String address;
 	private short sex;
-		
+
 	public Long getId() {
 		return id;
 	}
@@ -57,6 +60,30 @@ public class User {
 
 	public void setSex(short sex) {
 		this.sex = sex;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
 	}
 
 	public User(Long id, String username, String password, String balance) {
